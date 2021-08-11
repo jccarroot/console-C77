44c4efbc84ae655458900f0282e9717f71675562
jccarroot <84319758+jccarroot@users.noreply.github.com>
2021-08-11 20:21:38 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("my firt console project");
+var first_name="Jack";
+var last_name="Naidoo";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=1;
+var b=2;
+var sum=a+b;
+console.log(sum);
+
+var sub=a-b;
+console.log(sub);
+
+var multi=a*b;
+console.log(multi);
+
+var div=a/b;
+console.log(div);
